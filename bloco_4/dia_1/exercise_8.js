55f31d1aa0f17cb535e4ad0d1ad0e25028047487
alailsonko <aalailson3@gmail.com>
2020-11-27 14:23:26 -0300

    exercise 8 dia 1

---
+++ bloco_4/dia_1/exercise_8.js
@@ -0,0 +1,25 @@
+function par(number) {
+   if(number % 2 === 0){
+     return true
+   } else {
+    return false
+   }
+}
+
+par(2)
+par(3)
+par(4)
+par(5)
+par(6)
+par(7)
+par(8)
+par(9)
+par(10)
+par(11)
+par(12)
+par(13)
+par(14)
+par(15)
+par(16)
+par(17)
+par(18)
